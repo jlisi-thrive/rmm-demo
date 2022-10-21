9de55be10d3922d1ea32e08f886f835d61457d3a
Justin Lisi <jlisi@thrivenetworks.com>
2022-10-21 11:27:45 -0400

    test

--- _modules/thrive-custom.py
+++ _modules/thrive-custom.py
@@ -2,6 +2,18 @@ import json
 from operator import itemgetter
 
 
+def windowsEventLogWatcher():
+    newestEvent = {}
+    with open('C:\Temp\winlogbeat\winlogbeat', encoding="utf8") as f:
+        for jsonObj in f:
+            eventDict = json.loads(jsonObj)
+            newestEvent = eventDict
+            break
+    eventOutput = json.dumps(newestEvent, separators=(',', ':'))
+    print(eventOutput)
+    return eventOutput
+
+
 def setDiskUsageGrain():
     usageStats = __salt__["disk.usage"]()
     __salt__["grains.setval"]("diskUsage", usageStats)
