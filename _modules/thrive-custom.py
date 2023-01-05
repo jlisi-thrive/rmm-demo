32b3972543fe1031a6055ffb99b94c818ab6cca1
jlisi-thrive <96087149+jlisi-thrive@users.noreply.github.com>
2023-01-05 10:04:43 -0500

    Update thrive-custom.py

--- _modules/thrive-custom.py
+++ _modules/thrive-custom.py
@@ -1,6 +1,6 @@
 import json
 import requests
-import pingparsing
+#import pingparsing
 from operator import itemgetter
 
 
@@ -29,27 +29,27 @@ def setDiskUsageGrain():
     __salt__["grains.setval"]("diskUsage", usageStats)
     return usageStats
 
-def setPingGrain():
-    destinations = ["8.8.8.8", "8.8.4.4", "thrive.service-now.com"]
-    ping_parser = pingparsing.PingParsing()
-    transmitter = pingparsing.PingTransmitter()
-    transmitter.count = 3
-    results = []
+# def setPingGrain():
+#     destinations = ["8.8.8.8", "8.8.4.4", "thrive.service-now.com"]
+#     ping_parser = pingparsing.PingParsing()
+#     transmitter = pingparsing.PingTransmitter()
+#     transmitter.count = 3
+#     results = []
 
-    for destination in destinations:
-        transmitter.destination = destination
-        result = transmitter.ping()
-        response = ping_parser.parse(result)
-        resObj = {
-            'destination': response.destination,
-            'min': response.rtt_min,
-            'max': response.rtt_max,
-            'avg': response.rtt_avg
-        }
-        results.append(resObj)
+#     for destination in destinations:
+#         transmitter.destination = destination
+#         result = transmitter.ping()
+#         response = ping_parser.parse(result)
+#         resObj = {
+#             'destination': response.destination,
+#             'min': response.rtt_min,
+#             'max': response.rtt_max,
+#             'avg': response.rtt_avg
+#         }
+#         results.append(resObj)
 
-    __salt__["grains.setval"]("pings", results)
-    return results
+#     __salt__["grains.setval"]("pings", results)
+#     return results
 
 
 def setHostUsageMetrics():
